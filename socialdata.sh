e2f164a26f2bbddb4146c3c4cac6552127803655
IncioMan <incio_man@live.it>
2019-02-19 12:16:25 +0100

    rename

---
+++ socialdata.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd "/Users/ainc/Google Drive/Universitet/Social Data And Visualization/"
+open -a "Google Chrome" https://github.com/suneman/socialdataanalysis2019/wiki
+jupyter notebook
