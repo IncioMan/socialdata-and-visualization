aa2b5c97f3b5a73745f8e83071f2724349c778f4
IncioMan <incio_man@live.it>
2019-02-24 19:40:27 +0100

    w3 ex 3

--- socialdata.sh
+++ socialdata.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cd "/Users/ainc/Google Drive/Universitet/Social Data And Visualization/"
 open -a "Google Chrome" https://github.com/suneman/socialdataanalysis2019/wiki
+open .
 jupyter notebook
